1f3f387bbb7df305aa0842366b2bd870ee6da880
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2024-02-24 14:18:08 -0300

    empezando proyecto

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Mitenis</h1>
+            <div>
+                <button>Raquetas</button>
+                <button>Zapatillas</button>
+                <button>indumentaria</button>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
